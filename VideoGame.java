7127b74ce9c6e90f4ae9ef18371c10fdae554858
andreigildore <andrei_gildore@dlsu.edu.ph>
2026-07-10 22:44:05 +0800

    Implement VideoGame class with properties and methods

--- VideoGame.java
+++ VideoGame.java
@@ -1,3 +1,59 @@
+import java.time.LocalDate;
+
 public class VideoGame {
-    
+    private String title;
+    private LocalDate dateAdded;
+    private String genre;
+    private int gameMode;
+    private int status;
+    private int rating;
+    private String review;
+    private int playtime;
+    private String developer;
+    private String platform;
+
+    public VideoGame(String title, LocalDate dateAdded, String genre, int gameMode, String developer, String platform) {
+        this.title = title;
+        this.dateAdded = dateAdded;
+        this.genre = genre;
+        this.gameMode = gameMode;
+        this.developer = developer;
+        this.platform = platform;
+        this.status = 0;
+        this.playtime = 0;
+    }
+
+    public void updateStatus(int newStatus) {
+        this.status = newStatus;
+    }
+
+    public void rate(int n) {
+        if (this.status == 2) {
+            this.rating = n;
+            System.out.println("Rating saved successfully.");
+        } else {
+            System.out.println("Error: Cannot rate an incomplete Video Game.");
+        }
+    }
+
+    public void review(String text) {
+        if (this.status == 2) {
+            this.review = text;
+            System.out.println("Review saved successfully.");
+        } else {
+            System.out.println("Error: Cannot review an incomplete Video Game.");
+        }
+    }
+
+    public String displayInfo() {
+        return "Video Game: " + title + " | Platform: " + platform + " | Playtime: " + playtime + " hrs | Status: " + status;
+    }
+
+    public String getTitle() {
+        return this.title;
+    }
+
+    public int getStatus() {
+        return this.status;
+    }
 }
