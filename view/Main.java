00b353b67e4eb81ff6434b8dfcb64d80057706f3
andreigildore <andrei_gildore@dlsu.edu.ph>
2026-08-03 00:54:26 +0800

    Add system look and feel initialization
    Set the system look and feel for the UI.

--- view/Main.java
+++ view/Main.java
@@ -3,9 +3,16 @@ package view;
 import controller.LibraryController;
 import model.Library;
 import javax.swing.SwingUtilities;
+import javax.swing.UIManager;
 
 public class Main {
     public static void main(String[] args) {
+        try {
+            UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+        } catch (Exception e) {
+            System.err.println("Failed to initialize system look and feel.");
+        }
+
         SwingUtilities.invokeLater(() -> {
             Library library = new Library();
             LibraryController controller = new LibraryController(library);
