06a21f61bd99f557a35c8e94e4aff84a0b1d5347
andreigildore <andrei_gildore@dlsu.edu.ph>
2026-08-03 04:18:50 +0800

    Implement Episodic interface in Anime class

--- model/Anime.java
+++ model/Anime.java
@@ -1,10 +1,11 @@
 package model;
 
-public class Anime extends MediaEntry {
+public class Anime extends MediaEntry implements Episodic {
     private String studio;
     private String creator;
     private int episodes;
     private boolean isSubbed;
+    private int[] episodeRatings; 
 
     public Anime(String title, String genre, int status, String studio, String creator, int episodes, boolean isSubbed) {
         super(title, genre, status);
@@ -12,6 +13,12 @@ public class Anime extends MediaEntry {
         this.creator = creator;
         this.episodes = episodes;
         this.isSubbed = isSubbed;
+        this.episodeRatings = new int[episodes]; 
     }
+
     @Override public String getTypeLabel() { return "Anime"; }
+    
+    @Override public int getEpisodeCount() { return episodes; }
+    @Override public int[] getEpisodeRatings() { return episodeRatings; }
+    @Override public void setEpisodeRating(int index, int rating) { this.episodeRatings[index] = rating; }
 }
