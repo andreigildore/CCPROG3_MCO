8d8202beadf71d39cf1b558040139e567a9db139
andreigildore <andrei_gildore@dlsu.edu.ph>
2026-08-03 01:22:56 +0800

    Refactor VideoGame class constructor and comments
    Removed unnecessary comments and simplified constructor.

--- model/VideoGame.java
+++ model/VideoGame.java
@@ -1,57 +1,15 @@
 package model;
 
-/**
- * Represents a video game entry in the user's library.
- */
 public class VideoGame extends MediaEntry {
     private int playtime;
     private String developer;
     private String platform;
 
-    /**
-     * Creates a new video game with default status of 0 (planned).
-     * 
-     * @param title the title of the video game
-     * @param genre the genre of the video game
-     * @param playtime the playtime of the video game in hours
-     * @param developer the developer/developer team of the video game
-     * @param platform the platform of the video game
-     * Pre-condition: None.
-     * Post-condition: A new VideoGame object is created with a planned status.
-     */
-    public VideoGame(String title, String genre, int initialStatus, int playtime, String developer, String platform) {
-        super(title, genre, initialStatus);
+    public VideoGame(String title, String genre, int status, int playtime, String developer, String platform) {
+        super(title, genre, status);
         this.playtime = playtime;
         this.developer = developer;
         this.platform = platform;
     }
-
-    /**
-     * Adds hours to the total playtime.
-     * 
-     * @param additionalPlaytime the additional playtime in hours
-     * @return the number of currrent playtime for the videogame if additionalPlaytime is valid. Else, it returns -1.
-     * Pre-condition: The additionalPlaytime must be greater than 0.
-     * Post-condition: The total playtime is increased by the additionalPlaytime.
-     */
-    public int addPlaytime(int additionalPlaytime) {
-        if (additionalPlaytime > 0) {
-            playtime += additionalPlaytime;
-            return playtime;
-        } else 
-            return -1;
-    }
-
-    public String getDeveloper() {
-        return developer;
-    }
-
-    public String getPlatform() {
-        return platform;
-    }
-
-    @Override
-    public String getTypeLabel() {
-        return "Video Game";
-    }
+    @Override public String getTypeLabel() { return "Video Game"; }
 }
