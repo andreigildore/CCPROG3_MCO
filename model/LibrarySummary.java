b20f2f8d8ebc3c76be4f67bd6d60f39683dc1c12
andreigildore <andrei_gildore@dlsu.edu.ph>
2026-08-03 01:28:59 +0800

    Add LibrarySummary class to model package

---
+++ model/LibrarySummary.java
@@ -0,0 +1,8 @@
+package model;
+
+public class LibrarySummary {
+    public int total = 0, planned = 0, inProgress = 0, completed = 0;
+    public int animeCount = 0, tvSeriesCount = 0, videoGameCount = 0;
+    public int musicAlbumCount = 0, musicSingleCount = 0;
+    public double averageRating = 0.0;
+}
