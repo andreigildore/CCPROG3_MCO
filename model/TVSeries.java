3b01d25820691e6488c4a2581b3107f620841f8a
andreigildore <andrei_gildore@dlsu.edu.ph>
2026-08-03 04:19:09 +0800

    Implement Episodic interface in TVSeries class

--- model/TVSeries.java
+++ model/TVSeries.java
@@ -1,15 +1,22 @@
 package model;
 
-public class TVSeries extends MediaEntry {
+public class TVSeries extends MediaEntry implements Episodic {
     private String creator;
     private int episodes;
     private boolean isRealityTV;
+    private int[] episodeRatings; 
 
     public TVSeries(String title, String genre, int status, String creator, int episodes, boolean isRealityTV) {
         super(title, genre, status);
         this.creator = creator;
         this.episodes = episodes;
         this.isRealityTV = isRealityTV;
+        this.episodeRatings = new int[episodes];
     }
+
     @Override public String getTypeLabel() { return "TV Series"; }
+    
+    @Override public int getEpisodeCount() { return episodes; }
+    @Override public int[] getEpisodeRatings() { return episodeRatings; }
+    @Override public void setEpisodeRating(int index, int rating) { this.episodeRatings[index] = rating; }
 }
